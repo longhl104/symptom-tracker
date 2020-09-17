24b010c2756ab5367763212a8a3c0103f473953d
Ainslie <asco8022@uni.sydney.edu.au>
2020-09-17 12:03:04 +1000

    added login (edward) and register (ainslie) templates

--- project/app.py
+++ project/app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 @app.route('/service-worker.js')
 def service_worker():
     return app.send_static_file('service-worker.js')

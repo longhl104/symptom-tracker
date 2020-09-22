2cc4ce4590a917c6ff999f1eba6bfbf296e693de
Harrison Le <hale6334@uni.sydney.edu.au>
2020-09-22 10:53:57 +1000

    Updated schema.sql

--- project/schema.sql
+++ project/schema.sql
@@ -1,6 +1,21 @@
-DROP SCHEMA IF EXISTS accountserver CASCADE;
-CREATE SCHEMA accountserver;
-SET search_path TO 'accountserver';
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP SCHEMA IF EXISTS tingleserver CASCADE;
+CREATE SCHEMA tingleserver;
+SET search_path TO 'tingleserver';
+
+ALTER SCHEMA tingleserver OWNER TO postgres;
+SET default_tablespace = '';
+SET default_table_access_method = heap;
 
 DROP TABLE IF EXISTS Account CASCADE;
 DROP TABLE IF EXISTS Patient CASCADE;
@@ -12,10 +27,11 @@ CREATE TABLE AccountType (
     account_type_name VARCHAR(100) NOT NULL
 );
 
+ALTER TABLE tingleserver."AccountType" OWNER TO postgres;
+
 CREATE TABLE Account(
     account_id SERIAL PRIMARY KEY,
     account_type_id INTEGER REFERENCES AccountType(account_type_id) NOT NULL,
-    ac_username TEXT NOT NULL,
     ac_password TEXT NOT NULL,
     ac_first_name TEXT NOT NULL,
     ac_last_name TEXT NOT NULL,
@@ -26,10 +42,18 @@ CREATE TABLE Account(
     ac_admin SMALLINT DEFAULT 0
 );
 
+ALTER TABLE ONLY tingleserver."Account"
+	ADD CONSTRAINT ac_email UNIQUE (username);
+COMMENT ON CONSTRAINT ac_email ON tingleserver."Account" IS 'Email must be unique';
+
+ALTER TABLE tingleserver."Account" OWNER TO postgres;
+
 CREATE TABLE Clinician (
     clinician_id SERIAL UNIQUE REFERENCES Account(account_id),
     professions TEXT
 );
+
+ALTER TABLE tingleserver."Clinician" OWNER TO postgres;
 
 CREATE TABLE Patient(
     patient_id SERIAL REFERENCES Account(account_id),
@@ -37,14 +61,17 @@ CREATE TABLE Patient(
     pt_clinician INTEGER NOT NULL REFERENCES Clinician(clinician_id)
 );
 
+ALTER TABLE tingleserver."Patient" OWNER TO postgres;
+
 CREATE TABLE Researcher (
     researcher_id SERIAL REFERENCES Account(account_id),
     research_assigned TEXT
 );
 
-CREATE OR REPLACE FUNCTION accountserver.addpatient(
+ALTER TABLE tingleserver."Researcher" OWNER TO postgres;
+
+CREATE OR REPLACE FUNCTION tingleserver.addpatient(
     type TEXT,
-    username TEXT,
     password TEXT,
     firstname TEXT,
     lastname TEXT,
@@ -57,17 +84,17 @@ CREATE OR REPLACE FUNCTION accountserver.addpatient(
 RETURNS int AS
 $BODY$
     WITH ins1 AS (
-        INSERT INTO accountserver.Patient (pt_clinician, symptoms_tracking)
+        INSERT INTO tingleserver.Patient (pt_clinician, symptoms_tracking)
         VALUES (assignedclinician, symptoms)
         RETURNING patient_id
         ), ins2 AS (
-            INSERT INTO accountserver.Account (ac_username, ac_password, ac_first_name, ac_last_name, ac_email, ac_age, ac_gender, ac_phone)
-            VALUES (username, password, firstname, lastname, email, age, gender, phone)
+            INSERT INTO tingleserver.Account (ac_password, ac_first_name, ac_last_name, ac_email, ac_age, ac_gender, ac_phone)
+            VALUES (password, firstname, lastname, email, age, gender, phone)
             RETURNING account_id
         ), ins3 AS (
-            INSERT INTO accountserver.AccountType (account_type_name)
+            INSERT INTO tingleserver.AccountType (account_type_name)
             VALUES (type)
         )
-		SELECT patient_id FROM accountserver.Patient;
+		SELECT patient_id FROM tingleserver.Patient;
 $BODY$
 LANGUAGE sql;

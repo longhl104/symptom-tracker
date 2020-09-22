d8372db95d7ebd3117a7213793268ef7f9a5b2b6
Campos1098 <ecam6631@uni.sydney.edu.au>
2020-09-22 10:20:07 +1000

    Removed old versions of register template

--- project/app.py
+++ project/app.py
@@ -13,7 +13,3 @@ def service_worker():
 @app.route('/login')
 def login():
     return render_template('login.html')
-
-@app.route('/register')
-def register():
-    return render_template('register.html')

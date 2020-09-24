79eaecac9832ddbc35eb8126e8dbbc975d5fdedb
Osama Bin Waseem <obin7252@uni.sydney.edu.au>
2020-09-24 20:02:58 +1000

    Fixed failing tests

--- project/tests/test_basic.py
+++ project/tests/test_basic.py
@@ -3,14 +3,9 @@ from app import app
 
 class BasicTestCase(unittest.TestCase):
     #! test methods' name must start with "test_"
-    def test_home(self):
-        tester = app.test_client(self)
-        response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 302)
-
     def test_login(self):
         tester = app.test_client(self)
-        response = tester.get('/login', content_type='html/text')
+        response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
     def test_forgot_password(self):

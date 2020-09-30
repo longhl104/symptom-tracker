2821f2f2fa309fbcf49a6c8f77688018b30fbf0e
Long Nguyen Duy <dngu0797@uni.sydney.edu.au>
2020-09-30 19:13:43 +1000

    add database test template

--- project/tests/test_database.py
+++ project/tests/test_database.py
@@ -1,35 +1,16 @@
 import unittest
+import testing.postgresql
 from app import app
 import database
 
 
 class DatabaseTest(unittest.TestCase):
     #! test methods' name must start with "test_"
-    def test_database_connect_simple_connection(self):
-        conn = database.database_connect();
-        self.assertIsNotNone(conn);
+    def setUp(self):
+        self.postgresql = testing.postgresql.Postgresql()
 
-    def test_database_connect_database_is_not_presented(self):
-        f = open('sample-config.ini', 'r')
-        back_up = f.read()
-        f.close()
-
-        content = """[DATABASE]
-host = 127.0.0.1
-user = postgres
-password = tingle12345
-secret_key = /TMVe0"Lw`hc*0I"""
-
-        f = open('sample-config.ini', 'w')
-        f.write(content)
-        f.close()
-
-        conn = database.database_connect()
-        self.assertIsNotNone(conn)
-
-        f = open('sample-config.ini', 'w')
-        f.write(back_up)
-        f.close()
+    def tearDown(self):
+        self.postgresql.stop()
 
 
 if __name__ == '__main__':

03a1a4d16bf4410e504789396808478cba6fa8c4
Qinglin Wang <qwan3440@uni.sydney.edu.au>
2020-09-30 11:57:01 +1000

    should use config.ini not sample

--- project/database.py
+++ project/database.py
@@ -12,7 +12,7 @@ def database_connect():
     """
     # Read the config file
     config = configparser.ConfigParser()
-    config.read('sample-config.ini')
+    config.read('config.ini')
     if 'database' not in config['DATABASE']:
         config['DATABASE']['database'] = config['DATABASE']['user']
 

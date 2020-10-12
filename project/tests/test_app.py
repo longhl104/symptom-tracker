0133bb22bd12a5770217202200380dfb2747bf22
Long Nguyen Duy <dngu0797@uni.sydney.edu.au>
2020-10-12 23:38:00 +1100

    add more tests

--- project/tests/test_app.py
+++ project/tests/test_app.py
@@ -1,5 +1,5 @@
 import unittest
-from app import app
+from app import app as tingle
 from unittest import mock
 from flask import url_for, request
 
@@ -11,17 +11,17 @@ class AppTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    
     #! test methods' name must start with "test_"
+
     def test_void(self):
         pass
-    
-    #add mock patch to the function you want to mock
+
+    # add mock patch to the function you want to mock
     @mock.patch('database.check_login')
     def test_login_no_data(self, db):
         # db.return_value = [{'ac_id': 12, 'ac_email': 'long', 'ac_password': '12345678', 'ac_firstname': 'Long',
         #                     'ac_lastname': 'Nguyen', 'ac_age': 22, 'ac_gender': 'male', 'ac_phone': '0415123456'}]
-        with app.test_client() as client:
+        with tingle.test_client() as client:
             # set the return value
             db.return_value = None
             response = client.post('/', data=dict(
@@ -34,8 +34,9 @@ class AppTest(unittest.TestCase):
 
     @mock.patch('database.check_login')
     def test_login_has_data(self, db):
-        with app.test_client() as client:
-            db.return_value = [{'ac_id': 12, 'ac_email': 'long', 'ac_password': '12345678', 'ac_firstname': 'Long',                    'ac_lastname': 'Nguyen', 'ac_age': 22, 'ac_gender': 'male', 'ac_phone': '0415123456'}]
+        with tingle.test_client() as client:
+            db.return_value = [{'ac_id': 12, 'ac_email': 'long', 'ac_password': '12345678', 'ac_firstname': 'Long',
+                                'ac_lastname': 'Nguyen', 'ac_age': 22, 'ac_gender': 'male', 'ac_phone': '0415123456'}]
             response = client.post('/', data=dict(
                 email='long',
                 password='12345678'
@@ -45,8 +46,7 @@ class AppTest(unittest.TestCase):
         pass
 
     def test_login_get_method(self):
-        with app.test_client() as client:
-            app.session = {}
+        with tingle.test_client() as client:
             response = client.get('/', follow_redirects=True)
             self.assertEqual(response.status_code, 200)
             self.assertEqual(request.path, '/')
@@ -54,27 +54,26 @@ class AppTest(unittest.TestCase):
 
     @mock.patch('database.add_patient')
     def test_register_post_method_failed(self, db):
-        with app.test_client() as client:
+        with tingle.test_client() as client:
             db.return_value = None
             response = client.post('/register', data={
                 'first-name': 'foo',
                 'last-name': 'bar',
-                'gender':'Male',
+                'gender': 'Male',
                 'age': '12',
                 'mobile-number': '04123456789',
-                'treatment':'A',
-                'email-address':'foo@bar.com',
-                'password':'12345678',
-                'consent':'yes'
-            }
-            , follow_redirects=True)
+                'treatment': 'A',
+                'email-address': 'foo@bar.com',
+                'password': '12345678',
+                'consent': 'yes'
+            }, follow_redirects=True)
             self.assertEqual(response.status_code, 200)
             self.assertEqual(request.path, '/patient/')
         pass
 
     @mock.patch('database.add_patient')
     def test_register_post_method_successful(self, db):
-        with app.test_client() as client:
+        with tingle.test_client() as client:
             db.return_value = 1
             response = client.post('/register', data={
                 'first-name': 'foo',
@@ -93,8 +92,8 @@ class AppTest(unittest.TestCase):
 
     @mock.patch('database.add_patient')
     def test_register_post_method_exception(self, db):
-        with app.test_client() as client:
-            db.return_value = 1
+        with tingle.test_client() as client:
+            db.side_effect = Exception('Database error')
             response = client.post('/register', data={
                 'first-name': 'foo',
                 'last-name': 'bar',
@@ -107,32 +106,31 @@ class AppTest(unittest.TestCase):
                 'consent': 'yes'
             }, follow_redirects=True)
             self.assertEqual(response.status_code, 200)
-            self.assertEqual(request.path, url_for('patient_dashboard'))
-        pass
-
-    def test_register_get_method_not_logged_in(self):
-        with app.test_client() as client:
-            app.session = {}
-            response = client.get('/register', follow_redirects=True)
-            self.assertEqual(response.status_code, 200)
             self.assertEqual(request.path, '/patient/')
         pass
 
     def test_register_get_method_logged_in(self):
-        with app.test_client() as client:
-            app.session = {'logged_in':True}
+        with tingle.test_client() as client:
             response = client.get('/register', follow_redirects=True)
             self.assertEqual(response.status_code, 200)
             self.assertEqual(request.path, '/patient/')
         pass
 
+    @mock.patch('app.session', {'logged_in': None})
+    def test_register_get_method_not_logged_in(self):
+        with tingle.test_client() as client:
+            response = client.get('/register', follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(request.path, '/register')
+        pass
+
     def test_patient_dashboard(self):
-        with app.test_client() as client:
-            app.session = {'logged_in':True}
+        with tingle.test_client() as client:
             response = client.get('/patient', follow_redirects=True)
             self.assertEqual(response.status_code, 200)
             self.assertEqual(request.path, '/patient/')
         pass
+
 
 if __name__ == '__main__':
     unittest.main()

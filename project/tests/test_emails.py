422fdf8dbd4f7e5405a59f2c888fcbe3f933dab4
Ainslie <asco8022@uni.sydney.edu.au>
2020-10-13 10:35:35 +1100

    test fix 2 for build

--- project/tests/test_emails.py
+++ project/tests/test_emails.py
@@ -3,7 +3,7 @@ import email_handler
 
 class EmailTests(unittest.TestCase):
     def test_email_setup(self):
-        message = email_handler.setup_email("test@address.com")
+        message = email_handler.setup_email("test@address.com", "key123")
         self.assertEqual(message['To'],"test@address.com")
         self.assertEqual(message['From'],"brainandmindcentre.usyd@gmail.com")
         self.assertEqual(message['Subject'],"Reset your password")
